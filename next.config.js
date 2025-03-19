c7be6458669283e4897f2b43cb4e0aaddac5c34c
khatuanlam <khatuanlam@gmail.com>
2025-03-19 23:07:35 +0700

    Update site.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
-  },
+    images: {
+        domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    },
+    env: {
+        NOTION_PAGE_ID: '1bbc213a5bb18038ac70cfbb1482ca58',
+        NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID: 'G-1A2B3C4D5E',
+        NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION: '',
+        NEXT_PUBLIC_NAVER_SITE_VERIFICATION: '',
+        NEXT_PUBLIC_UTTERANCES_REPO: ''
+    },
 }
