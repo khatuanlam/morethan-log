73af197344b46349155140dd804d32d7db58b11b
khatuanlam <khatuanlam@gmail.com>
2025-03-20 13:57:05 +0700

    'update'

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,52 +1,53 @@
-import styled from "@emotion/styled"
-import { useRouter } from "next/router"
-import React from "react"
-import { colors } from "src/styles"
-import { COLOR_SET } from "./constants"
+import { CSSObject } from "@emotion/react"; // Import CSSObject for typing
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import { colors } from "src/styles";
+import { COLOR_SET } from "./constants";
 
 export const getColorClassByName = (name: string): string => {
   try {
-    let sum = 0
-    name.split("").forEach((alphabet) => (sum = sum + alphabet.charCodeAt(0)))
+    let sum = 0;
+    name.split("").forEach((alphabet) => (sum = sum + alphabet.charCodeAt(0)));
     const colorKey = sum
       .toString(16)
-      ?.[sum.toString(16).length - 1].toUpperCase()
-    return COLOR_SET[colorKey]
+      ?.[sum.toString(16).length - 1].toUpperCase();
+    return COLOR_SET[colorKey];
   } catch {
-    return COLOR_SET[0]
+    return COLOR_SET[0];
   }
-}
+};
 
 type Props = {
-  children?: React.ReactNode
-  readOnly?: boolean
-}
+  children?: ReactNode;
+  readOnly?: boolean;
+};
 
 const Category = ({ readOnly = false, children }: Props) => {
-  const router = useRouter()
+  const router = useRouter();
 
   const handleClick = (value?: string) => {
-    if (readOnly) return
-    router.push(`/?category=${value}`)
-  }
+    if (readOnly) return;
+    router.push(`/?category=${value}`);
+  };
 
   return (
     <StyledWrapper
-      onClick={() => typeof children === 'string' && handleClick(children)}
+      onClick={() => typeof children === "string" && handleClick(children)}
       css={{
-        backgroundColor: typeof children === 'string' ? getColorClassByName(children) : undefined,
+        backgroundColor:
+          typeof children === "string" ? getColorClassByName(children) : undefined,
         cursor: readOnly ? "default" : "pointer",
       }}
     >
       {children}
     </StyledWrapper>
-  )
-}
+  );
+};
 
+export default Category;
 
-export default Category
-
-const StyledWrapper = styled.div`
+const StyledWrapper = styled.div<{ css?: CSSObject }>`
   padding-top: 0.25rem;
   padding-bottom: 0.25rem;
   padding-left: 0.5rem;
@@ -57,4 +58,4 @@ const StyledWrapper = styled.div`
   line-height: 1.25rem;
   opacity: 0.9;
   color: ${colors.dark.gray1};
-`
+`;
